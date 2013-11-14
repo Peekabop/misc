507d10f0471741e9f9fa73b8942dc14f0e9e1d53
Bo Potts <peekabop85@gmail.com>
2013-11-14 16:39:27 -0600

    my first commit

---
+++ dev.bat
@@ -0,0 +1,16 @@
+@echo off
+
+echo "WARNING, FIREFOX WEB DEVELOPER TOOLBAR NOT INCLUDED!"
+pause
+
+@powershell -NoProfile -ExecutionPolicy unrestricted -Command "iex ((new-object net.webclient).DownloadString('https://chocolatey.org/install.ps1'))" && SET PATH=%PATH%;%systemdrive%\chocolatey\bin
+pause
+
+call cinst -force firefox
+call cinst -force gow
+call cinst -force vim
+echo "Programs Have Been Installed."
+pause
+
+cd %HOMEPATH%\Program Files (x86)\Mozilla Firefox
+start firefox.exe
