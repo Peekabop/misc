499c483db26454931401b46c13b9d08598cd4093
Bo <peekabop85@gmail.com>
2013-11-14 19:41:28 -0600

    Update dev.bat
    new file in new repository added, changes to make this more awesome are welcome

--- dev.bat
+++ dev.bat
@@ -1,6 +1,6 @@
 @echo off
 
-echo "WARNING, FIREFOX WEB DEVELOPER TOOLBAR NOT INCLUDED!"
+
 pause
 
 @powershell -NoProfile -ExecutionPolicy unrestricted -Command "iex ((new-object net.webclient).DownloadString('https://chocolatey.org/install.ps1'))" && SET PATH=%PATH%;%systemdrive%\chocolatey\bin
